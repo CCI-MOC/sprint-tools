37cbd911ce9295ffd98bfc366ccc24faf59edff6
Lars Kellogg-Stedman <lars@redhat.com>
2022-06-22 11:11:25 -0400

    Add "In Review" column
    Add a column for tasks that are either waiting for review or are currently
    under review.

--- moc_sprint_tools/defaults.py
+++ moc_sprint_tools/defaults.py
@@ -1,5 +1,5 @@
 default_organization = "CCI-MOC"
-default_sprint_columns = ["Notes", "Sprint Backlog", "In Progress", "Done"]
+default_sprint_columns = ["Notes", "Sprint Backlog", "In Progress", "In Review", "Done"]
 default_skip_copy = ["notes", "done"]
 default_sprint_notes_repo = "sprint-notes"
 default_days_done = 14
